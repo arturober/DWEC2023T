10aa44b6fc7ed00c2c6dcbef76be560a7a40e19c
Arturo Bernal <arturober@gmail.com>
2023-09-28 16:32:41 +0200

    dom

--- 1 - JavaScript/Parte 2/5.dom.js
+++ 1 - JavaScript/Parte 2/5.dom.js
@@ -2,23 +2,30 @@
 
 const list = document.getElementById("list");
 const descInput = document.getElementById("description");
+const posInput = document.getElementById("position");
 const lastBtn = document.getElementById("addLast");
 const firstBtn = document.getElementById("addFirst");
 const afterBtn = document.getElementById("addAfter");
 const clearBtn = document.getElementById("clear");
 
 firstBtn.addEventListener("click", e => {
-
+    const li = document.createElement("li");
+    li.append(descInput.value);
+    list.prepend(li);
 });
 
 lastBtn.addEventListener("click", e => {
-
+    const li = document.createElement("li");
+    li.append(descInput.value);
+    list.append(li);
 });
 
 afterBtn.addEventListener("click", e => {
-
+    const li = document.createElement("li");
+    li.append(descInput.value);
+    list.children[posInput.value]?.after(li);
 });
 
 clearBtn.addEventListener("click", e => {
-
+    list.replaceChildren();
 });
