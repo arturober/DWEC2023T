b98e2fd8d0a597a2a2c0bf1262414c84280bf571
Arturo Bernal <arturober@gmail.com>
2023-11-09 15:57:33 +0100

    ts

---
+++ 1 - JavaScript/Parte 5/vite-ts/src/main.ts
@@ -0,0 +1,14 @@
+let nombre: string;
+nombre = "juan";
+
+console.log(nombre);
+
+const a: number[] = [];
+a.push(23);
+
+
+function suma(n1: number, n2: number): number {
+    if(n1 > 0)
+        return n1 + n2;
+    return 0;
+}
