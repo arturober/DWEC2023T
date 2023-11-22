5e1275d0b8ed91abb443b2864119888689599e3f
Arturo Bernal <arturober@gmail.com>
2023-11-22 17:28:38 +0100

    maps

--- 1 - JavaScript/Parte 5/maps-ts/src/map-service.ts
+++ 1 - JavaScript/Parte 5/maps-ts/src/map-service.ts
@@ -16,19 +16,22 @@ export class MapService {
     static #loadBingApi(): Promise<void> {
         this.#apiLoading = true;
         const script = document.createElement("script");
-        script.src = "https://www.bing.com/api/maps/mapcontrol?callback=showMap";
+        script.src =
+            "https://www.bing.com/api/maps/mapcontrol?callback=showMap";
         script.defer = true;
         document.body.append(script);
 
         return new Promise<void>((resolve, reject) => {
-            (window as unknown as {showMap: () => void}).showMap = (): void => {
-                this.#apiLoaded = true;
-                resolve();
-            };
-            script.addEventListener("error", () => reject("Bing API bould not be loaded"));
+            (window as unknown as { showMap: () => void }).showMap =
+                (): void => {
+                    this.#apiLoaded = true;
+                    resolve();
+                };
+            script.addEventListener("error", () =>
+                reject("Bing API bould not be loaded"),
+            );
         });
     }
-
 
     static async loadAutoSuggest(): Promise<void> {
         return new Promise<void>((resolve) => {
@@ -43,37 +46,45 @@ export class MapService {
         return this.#autoSuggestLoaded;
     }
 
-
     static async createMapService(
         coords: Coordinates,
-        divMapId: string
+        divMapId: string,
     ): Promise<MapService> {
-        if(!this.#apiLoaded) await this.#loadBingApi();
-        else if(!this.#apiLoading) throw Error("API is already being loaded elsewhere");
+        if (!this.#apiLoaded) await this.#loadBingApi();
+        else if (!this.#apiLoading)
+            throw Error("API is already being loaded elsewhere");
 
         const mapService = new MapService();
         mapService.#map = new Microsoft.Maps.Map("#" + divMapId, {
             credentials: API_KEY,
-            center: new Microsoft.Maps.Location(coords.latitude, coords.longitude),
+            center: new Microsoft.Maps.Location(
+                coords.latitude,
+                coords.longitude,
+            ),
             mapTypeId: Microsoft.Maps.MapTypeId.road,
-            zoom: 13
+            zoom: 13,
         });
         return mapService;
     }
 
-    createMarker(
-        coords: Coordinates,
-        color = "red"
-    ): Microsoft.Maps.Pushpin {
-        const pin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(coords.latitude, coords.longitude), { color });
+    createMarker(coords: Coordinates, color = "red"): Microsoft.Maps.Pushpin {
+        const pin = new Microsoft.Maps.Pushpin(
+            new Microsoft.Maps.Location(coords.latitude, coords.longitude),
+            { color },
+        );
         this.map.entities.push(pin);
 
         return pin;
     }
 
     async getAutoSuggestManager(): Promise<Microsoft.Maps.AutosuggestManager> {
-        if(!MapService.autoSuggestLoaded) await MapService.loadAutoSuggest();
-        return new Microsoft.Maps.AutosuggestManager({map: this.#map, placeSuggestions: true, addressSuggestions: true, businessSuggestions: true} as AutoSuggestOptions);
+        if (!MapService.autoSuggestLoaded) await MapService.loadAutoSuggest();
+        return new Microsoft.Maps.AutosuggestManager({
+            map: this.#map,
+            placeSuggestions: true,
+            addressSuggestions: true,
+            businessSuggestions: true,
+        } as AutoSuggestOptions);
     }
 
     get map(): Microsoft.Maps.Map {
