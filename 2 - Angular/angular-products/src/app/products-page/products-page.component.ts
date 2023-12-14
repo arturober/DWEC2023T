14cabcd3c78f568dda3266e3b0fd0fd47793b94f
Arturo Bernal <arturober@gmail.com>
2023-12-14 15:57:05 +0100

    products

--- 2 - Angular/angular-products/src/app/products-page/products-page.component.ts
+++ 2 - Angular/angular-products/src/app/products-page/products-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from '../interfaces/product';
 
 @Component({
   selector: 'products-page',
@@ -8,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrl: './products-page.component.css'
 })
 export class ProductsPageComponent {
+  title = "Mi lista de productos";
 
+  products: Product[] = [{
+    id: 1,
+    description: 'Pollo de goma gritón',
+    available: '2016-10-03',
+    price: 75,
+    imageUrl: 'assets/chicken.jpg',
+    rating: 5
+  },{
+    id: 2,
+    description: 'Mano auxiliar',
+    available: '2016-09-15',
+    price: 96.95,
+    imageUrl: 'assets/hand.jpg',
+    rating: 4
+  }];
 }
