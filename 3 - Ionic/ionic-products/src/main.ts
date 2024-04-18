51ab0f27542ac2b91d6218b60eb730deadd9767b
Arturo Bernal <arturo@email.com>
2024-04-18 22:38:17 +0200

    ionic

---
+++ 3 - Ionic/ionic-products/src/main.ts
@@ -0,0 +1,24 @@
+import { enableProdMode } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { PreloadAllModules, PreloadingStrategy, RouteReuseStrategy, provideRouter, withComponentInputBinding, withPreloading, withRouterConfig } from '@angular/router';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+
+import { routes } from './app/app.routes';
+import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { baseUrlInterceptor } from './app/interceptors/base-url.interceptor';
+import { authTokenInterceptor } from './app/interceptors/auth-token.interceptor';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideIonicAngular(),
+    provideRouter(routes, withPreloading(PreloadAllModules), withComponentInputBinding(), withRouterConfig({paramsInheritanceStrategy: 'always'})),
+    provideHttpClient(withInterceptors([baseUrlInterceptor, authTokenInterceptor])),
+  ],
+});
